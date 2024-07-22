98df7d4a948330f666115f16eba8d7131f16ca02
Your Name <you@example.com>
2024-07-22 21:40:15 +0530

    first commit

--- src/Assignment.js
+++ src/Assignment.js
@@ -1,16 +1,33 @@
+
 import React from "react";
-function Assignment(){
+import './Assignment.css';
+
+function Assignment() {
+    const data = [
+        { imgSrc: "images.jpg", name: "Sowji", feedback: "Product is bad" },
+        { imgSrc: "images3.png", name: "Keerthi", feedback: "GOOD" },
+        { imgSrc: "images4.png", name: "Suji", feedback: "excellent" },
+        { imgSrc: "images5.png",  name: "Navya", feedback:"Good"},
+        {imgSrc:  "images6.png",  name:"PRIYA", feedback:"average"},
+        { imgSrc: "images4.png", name: "Sowmya", feedback: "excellent" },
+    ];
+
+
     return (
-        <div>
-
-          <img src="images.jpg"></img>
-           <ul type="none">
-            <li>Review name</li>
-            <li>Product is bad</li>
-           </ul>
-        
+        <div className="assignment-container">
+            <ul className="list">
+                {data.map((item, index) => (
+                    <li key={index} className="list-item">
+                        <img src={item.imgSrc} alt={item.name} className="item-image"/>
+                        <div className="item-details">
+                            <span className="item-name">{item.name}</span>
+                            <span className="item-feedback">{item.feedback}</span>
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </div>
-    )
-
+    );
 }
+
 export default Assignment;
--- src/index.js
+++ src/index.js
@@ -17,10 +17,11 @@
 // reportWebVitals();
 import React from "react";
 import ReactDOM  from "react-dom/client";
+// import Assignment from "./Assignment";
 //  import Two from "./Two";
 // import Fifth from "./Fourth"
 //import Fifth from "./Fifth";
-import Review from "./Review";
+ import Review from "./Review";
 const rootElement = document.getElementById('root')
 const rootRef=ReactDOM.createRoot(rootElement);
 rootRef.render(<Review/>)
